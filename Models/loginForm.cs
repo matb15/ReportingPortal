0e6aa11c1ce5a06846de3cc43d4d91eb2317ad6a
T1 Faker <fabio04musitelli@gmail.com>
2025-05-09 17:44:40 +0200

    feat: implement login functionality with form validation and UI updates

---
+++ Models/loginForm.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        public string Password { get; set; } = default!;
+    }
+}
