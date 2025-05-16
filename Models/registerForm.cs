91ccff6e8b60912f2dcd4e6f9d6f130a3e479def
T1 Faker <fabio04musitelli@gmail.com>
2025-05-16 14:09:37 +0200

    feat: implement registration and login forms with validation

---
+++ Models/registerForm.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        public string Password { get; set; } = default!;
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = default!;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        public string Name { get; set; } = default!;
+        [Required]
+        [StringLength(100, ErrorMessage = "Surname cannot be longer than 100 characters.")]
+        public string Surname { get; set; } = default!;
+    }
+}
