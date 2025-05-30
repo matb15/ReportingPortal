a64f0ed522eb5960490c7d22b01b99630df384be
oogway03 <Nmola2003@gmail.com>
2025-05-30 15:47:08 +0200

    refactor: simplify HomeController by removing unused dependencies and notification logic

--- ReportingPortalServer/Controllers/HomeController.cs
+++ ReportingPortalServer/Controllers/HomeController.cs
@@ -5,19 +5,12 @@ namespace ReportingPortalServer.Controllers
 {
     [ApiController]
     [Route("/")]
-    public class HomeController(INotificationService notificationService, ApplicationDbContext applicationDbContext, IConfiguration configuration) : ControllerBase
+    public class HomeController : ControllerBase
     {
-        private readonly INotificationService _notificationService = notificationService;
-        private readonly ApplicationDbContext _applicationDbContext = applicationDbContext;
-        private readonly IConfiguration _configuration = configuration;
-
         [HttpGet]
         public IActionResult GetStatus()
         {
-            _notificationService.SendNotificationPushUser(2, "Test notification", _applicationDbContext, _configuration);
-
             return Ok(new { status = "API is running." });
-
         }
     }
 }
