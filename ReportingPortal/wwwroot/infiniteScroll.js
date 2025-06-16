8739035e00a351793d8db0a75ebc247a9ddb1264
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2025-06-16 11:02:53 +0200

    feat(infinite scroll)

---
+++ ReportingPortal/wwwroot/infiniteScroll.js
@@ -0,0 +1,12 @@
+﻿window.infiniteScroll = {
+    observeScroll: function (dotNetHelper, elementId) {
+        const el = document.getElementById(elementId);
+        if (!el) return;
+
+        el.addEventListener('scroll', () => {
+            if (el.scrollTop + el.clientHeight >= el.scrollHeight - 50) {
+                dotNetHelper.invokeMethodAsync('LoadMoreReports');
+            }
+        });
+    }
+};
